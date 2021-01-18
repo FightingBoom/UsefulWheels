6ec2a53c3ed53198d6a290525f61712b1f35c9f7
FightingBoom <1446227671@qq.com>
2021-01-18 23:00:34 +0800

    :sparkles:增加模块测试代码

--- 11-C/CWheels.h
+++ 11-C/CWheels.h
@@ -1,10 +1,18 @@
 #ifndef __CWHEELS_H__
 #define __CWHEELS_H__
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 // HEX to BCD(8421)
 unsigned char HexToBcd(unsigned char ucHex);
 
 // BCD(8421) to HEX
-unsigned char BcdToHex(unsigned char ucBcd)
+unsigned char BcdToHex(unsigned char ucBcd);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
---
+++ 11-C/ModulTest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "CWheels.h"
+
+extern unsigned char HexToBcd(unsigned char ucHex);
+extern unsigned char BcdToHex(unsigned char ucBcd);
+
+int main(void)
+{
+    unsigned char ucTemp = 0x32;
+    unsigned char ucResult = 0;
+
+    printf("ucTemp = 0x%02X\n", ucTemp);
+
+    ucResult = HexToBcd(ucTemp);
+    printf("ucResult = 0x%02X\n", ucResult);
+
+    ucResult = BcdToHex(ucResult);
+    printf("ucResult = 0x%02X\n", ucResult);
+
+}
