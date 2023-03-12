4c9bfffb6c1ee947bbf96e21182998c8e43695b6
FightingBoom <1446227671@qq.com>
2023-03-12 18:00:29 +0800

    add moduletest demo. test string contact.

--- include/commondefine.h
+++ include/commondefine.h
@@ -1,7 +1,7 @@
 #ifndef _COMMONDEFINE_H_
 #define _COMMONDEFINE_H_
 
-#define STRING_CONTACT(x,y) x##y    //拼接
+#define STRING_CONTACT(x,y) #x#y    //拼接
 #define STRING_TOSTR(x) #x          //转字符串
 
 #endif
---
+++ moduletest/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../include/commondefine.h"
+
+int main()
+{
+    printf("common define test start...\n");
+    printf("test %s\n", STRING_CONTACT(A,B));
+
+    printf("------------------------\n");
+
+    printf("test %s\n", STRING_TOSTR(str?));
+    printf("common define test end...\n");
+    // while(1) ; // Windows环境CMD窗口测试，保持显示
+}
+
+
+
