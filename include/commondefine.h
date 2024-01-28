579c1bb3fbf22d015652b8c7770cbd685738cd89
FightingBoom <1446227671@qq.com>
2024-01-28 15:04:51 +0800

    新增计算最小值通用宏定义

--- include/commondefine.h
+++ include/commondefine.h
@@ -1,7 +1,10 @@
 #ifndef _COMMONDEFINE_H_
 #define _COMMONDEFINE_H_
 
-#define STRING_CONTACT(x,y) #x#y    //拼接
+#define STRING_CONTACT(x, y) #x#y   //拼接
 #define STRING_TOSTR(x) #x          //转字符串
+
+// UsefulWheels Common
+#define UWC_MIN(a, b) (((a) <= (b)) ? (a) : (b))
 
 #endif
--- moduletest/main.cpp
+++ moduletest/main.cpp
@@ -4,11 +4,16 @@
 int main()
 {
     printf("common define test start...\n");
-    printf("test %s\n", STRING_CONTACT(A,B));
+    printf("test %s\n", STRING_CONTACT(A, B));
 
     printf("------------------------\n");
-
     printf("test %s\n", STRING_TOSTR(str?));
+
+    printf("------------------------\n");
+    printf("min %d\n", UWC_MIN(0, 1));
+    printf("min %d\n", UWC_MIN(0, -1));
+    printf("min %d\n", UWC_MIN(0, 0));
+
     printf("common define test end...\n");
     // while(1) ; // Windows环境CMD窗口测试，保持显示
 }
