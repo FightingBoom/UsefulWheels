d306f2bf1b81e323bf37a38ddc64547197f1eb80
FightingBoom <1446227671@qq.com>
2024-01-28 16:17:40 +0800

    新增千米/小时与英里/小时的互相转换

--- include/commondefine.h
+++ include/commondefine.h
@@ -8,4 +8,11 @@
 #define UWC_MIN(a, b) (((a) <= (b)) ? (a) : (b))
 #define UWC_MAX(a, b) (((a) >= (b)) ? (a) : (b))
 
+// 速度单位
+// 英里每小时（miles per hour，缩写为 mph）
+// 公里每小时（kilometers per hour，缩写为 km/h 或 kph）
+// 1 英里/小时 = 1.60934 公里/小时, 1 公里/小时 ≈ 0.62137 英里/小时
+#define UWC_KPH_TO_MPH(a) ((a) / (1.60934))
+#define UWC_MPH_TO_KPH(a) ((a) * (1.60934))
+
 #endif
--- moduletest/main.cpp
+++ moduletest/main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 #include "../include/commondefine.h"
 
+void KphToMphDemo()
+{
+    printf("------------------------\n");
+    printf("kph to mph %f\n", UWC_KPH_TO_MPH(80));
+    int kmTemp = 80;
+    int mileTemp = UWC_KPH_TO_MPH(kmTemp);
+    printf("mph int %d\n", mileTemp);
+    float mileTempFloat = UWC_KPH_TO_MPH(kmTemp);
+    printf("mph float %f\n", mileTempFloat);
+    double mileTempDouble = UWC_KPH_TO_MPH(kmTemp);
+    printf("mph double %f\n", mileTempDouble);
+    printf("------------------------\n");
+
+/*
+------------------------
+kph to mph 49.709819
+mph int 49
+mph float 49.709820
+mph double 49.709819
+------------------------
+*/
+}
+
+void MphToKphDemo()
+{
+    printf("------------------------\n");
+    printf("mph to kph %f\n", UWC_MPH_TO_KPH(80));
+    int mileTemp = 80;
+    int kmTemp = UWC_MPH_TO_KPH(mileTemp);
+    printf("kph int %d\n", kmTemp);
+    float kmTempFloat = UWC_MPH_TO_KPH(mileTemp);
+    printf("kph float %f\n", kmTempFloat);
+    double kmTempDouble = UWC_MPH_TO_KPH(mileTemp);
+    printf("kph double %f\n", kmTempDouble);
+    printf("------------------------\n");
+
+/*
+------------------------
+mph to kph 128.747200
+kph int 128
+kph float 128.747192
+kph double 128.747200
+------------------------
+*/
+}
+
 int main()
 {
     printf("common define test start...\n");
@@ -22,6 +68,12 @@ int main()
     printf("max %d\n", UWC_MAX(0, 0));
     printf("max %d\n", UWC_MAX(-10, -50));
     printf("max %d\n", UWC_MAX(10, 40));
+
+    // 千米/小时 -> 英里/小时
+    KphToMphDemo();
+
+    // 英里/小时 -> 千米/小时
+    MphToKphDemo();
 
     printf("common define test end...\n");
     // while(1) ; // Windows环境CMD窗口测试，保持显示
